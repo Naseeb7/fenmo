957913ecf8431c070900ee9f4464764fcf4b2308
Pradosh <chandpradosh7@gmail.com>
2026-04-21 12:33:25 +0530

    refactor: exclude mongodb internal fields from api responses

--- utils/expenseResponse.ts
+++ utils/expenseResponse.ts
@@ -1,7 +1,11 @@
-export function sanitizeExpense<T extends { idempotencyKey?: string }>(
+export function sanitizeExpense<T extends { idempotencyKey?: string; __v?: number }>(
   expense: T
-): Omit<T, "idempotencyKey"> {
-  const { idempotencyKey: _idempotencyKey, ...sanitizedExpense } = expense;
+): Omit<T, "idempotencyKey" | "__v"> {
+  const {
+    idempotencyKey: _idempotencyKey,
+    __v: _version,
+    ...sanitizedExpense
+  } = expense;
 
   return sanitizedExpense;
 }
